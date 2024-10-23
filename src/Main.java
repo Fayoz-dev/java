ab182a1530cb822e2e26f463f4060990d21f7b79
Fayoz-dev <frasulov988@gmail.com>
2024-10-23 20:30:33 +0500

    commits

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+
+public class Main {
+    static int a = 5;
+    static int b = 10;
+    static boolean c = true;
+    static boolean d = false;
+    static float e = 0.5f;
+    static float f = 3.251f;
+    static String firstName = "Rasulov";
+    static String lastName = "Fayoz";
+    static String age = "24";
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(age);
+
+        char letterA = 'A', letterB = 'B', letterC = 'C', letterD = 'D', letterE = 'E';
+        char letterF = 'F', letterG = 'G', letterH = 'H', letterI = 'I', letterJ = 'J';
+        char letterK = 'K', letterL = 'L', letterM = 'M', letterN = 'N', letterO = 'O';
+        char letterP = 'P', letterQ = 'Q', letterR = 'R', letterS = 'S', letterT = 'T';
+        char letterU = 'U', letterV = 'V', letterW = 'W', letterX = 'X', letterY = 'Y', letterZ = 'Z';
+
+        char lettera = 'a', letterb = 'b', letterc = 'c', letterd = 'd', lettere = 'e';
+        char letterf = 'f', letterg = 'g', letterh = 'h', letteri = 'i', letterj = 'j';
+        char letterk = 'k', letterl = 'l', letterm = 'm', lettern = 'n', lettero = 'o';
+        char letterp = 'p', letterq = 'q', letterr = 'r', letters = 's', lettert = 't';
+        char letteru = 'u', letterv = 'v', letterw = 'w', letterx = 'x', lettery = 'y', letterz = 'z';
+
+        System.out.println(letterA + " " + letterB + " " + letterC + " " + letterD + " " + letterE + " "
+                + letterF + " " + letterG + " " + letterH + " " + letterI + " " + letterJ + " "
+                + letterK + " " + letterL + " " + letterM + " " + letterN + " " + letterO + " "
+                + letterP + " " + letterQ + " " + letterR + " " + letterS + " " + letterT + " "
+                + letterU + " " + letterV + " " + letterW + " " + letterX + " " + letterY + " " + letterZ);
+
+        System.out.println(lettera + " " + letterb + " " + letterc + " " + letterd + " " + lettere + " "
+                + letterf + " " + letterg + " " + letterh + " " + letteri + " " + letterj + " "
+                + letterk + " " + letterl + " " + letterm + " " + lettern + " " + lettero + " "
+                + letterp + " " + letterq + " " + letterr + " " + letters + " " + lettert + " "
+                + letteru + " " + letterv + " " + letterw + " " + letterx + " " + lettery + " " + letterz);
+    }
+
+}
