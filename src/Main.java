08e6eb3adcd182a3e7416514453ee8395355a98f
Fayoz-dev <frasulov988@gmail.com>
2024-10-24 22:05:39 +0500

    switch new sntax

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int y = 7;
+        int y = 3;
         switch (y) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("ish kuni");
-                break;
-            case 6:
-            case 7:
-                System.out.println("Dam olish kuni");
-
+            case 1 -> System.out.println("Hello");
+            case 2 -> System.out.println("World");
+            case 3 -> System.out.println("Hello");
+            case 4 -> System.out.println("World");
+            case 5 -> System.out.println("Hello");
         }
     }
 
