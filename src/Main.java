06f3ec97678880aa639fc61b19a53f147d47f10f
Fayoz-dev <frasulov988@gmail.com>
2024-10-24 21:57:47 +0500

    switch joined cases

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       int y= 5;
-       System.out.println(y>3 || y<4);
+        int y = 7;
+        switch (y) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("ish kuni");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Dam olish kuni");
+
+        }
     }
 
 }
