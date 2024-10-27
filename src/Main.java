f36609a970c0b37444f1d8e66896ecf7cea38bd5
Fayoz-dev <frasulov988@gmail.com>
2024-10-27 11:25:29 +0500

    tub son

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int y = 3;
-        switch (y) {
-            case 1 -> System.out.println("Hello");
-            case 2 -> System.out.println("World");
-            case 3 -> System.out.println("Hello");
-            case 4 -> System.out.println("World");
-            case 5 -> System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Biror son kiriting: ");
+        int n = sc.nextInt();
+        int count = 0;
+     for (int i=1; i<=n; i++){
+         if (n % i == 0){
+             count++;
+         }
+     }
+        if (count == 2){
+            System.out.println("Tub son");
+        }else {
+            System.out.println("Bu tub son emas,chunki bo'luvchilari :" + count +"ta");
         }
     }
-
 }
