896d9b2aded2e357b50384065a1b621f71244e13
Fayoz-dev <frasulov988@gmail.com>
2024-10-27 20:51:26 +0500

    raqamla yigindisi

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Biror son kiriting: ");
-        int n = sc.nextInt();
-        int count = 0;
-     for (int i=1; i<=n; i++){
-         if (n % i == 0){
-             count++;
-         }
-     }
-        if (count == 2){
-            System.out.println("Tub son");
-        }else {
-            System.out.println("Bu tub son emas,chunki bo'luvchilari :" + count +"ta");
-        }
-    }
+        int n = 35;
+        int sum = 0;
+        int b;
+            for ( ; n>0; n/=10){
+                b = n%10;
+                sum+=b;
+
+            }
+        System.out.println(sum);
+       }
 }
