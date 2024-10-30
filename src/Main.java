45f994a8d22a94e5a45e10d05cd593bef63401fa
Fayoz-dev <frasulov988@gmail.com>
2024-10-30 21:51:25 +0500

    feat: 1 + 22 + 333 + 4444 + 55555 = 60355

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 35;
-        int sum = 0;
-        int b;
-            for ( ; n>0; n/=10){
-                b = n%10;
-                sum+=b;
-
+        int n = 5;
+        int sum=0;
+        for (int i = 1; i <= n; i++) {
+            int num=0;
+            for (int j = 1; j <= i; j++) {
+                num = num*10 + i;
             }
-        System.out.println(sum);
-       }
+            System.out.print(num);
+            sum+=num;
+            if (i!=n){
+                System.out.print(" + ");
+            }
+        }
+        System.out.println(" = "+sum);
+
+    }
 }
