d2f9b50400aeb73eef7d75c0913feeb6005aa95d
Fayoz-dev <frasulov988@gmail.com>
2024-10-31 22:59:17 +0500

    practics 3

---
+++ src/practics/Practisc3.java
@@ -0,0 +1,22 @@
+package practics;
+
+public class Practisc3 {
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            System.out.print("(0," + j + ") ");
+        }
+        System.out.println();
+
+        for (int i = 1; i < 9; i++) {
+            for (int j = 0; j < 9; j=j+2) {
+                System.out.print("("+i+","+j+")");
+            }
+            System.out.println();
+        }
+
+        for (int j = 0; j < 10; j++) {
+            System.out.print("(9," + j + ") ");
+        }
+        System.out.println();
+    }
+}
