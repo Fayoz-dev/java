e9997cb754882a18f9440a8f8e91572ca28f531e
Fayoz-dev <frasulov988@gmail.com>
2024-10-31 23:10:00 +0500

    practics 5

---
+++ src/practics/Practisc5.java
@@ -0,0 +1,15 @@
+package practics;
+
+public class Practisc5 {
+    public static void main(String[] args) {
+        for (int j = 0; j < 100; j=j+10) {
+            System.out.print(j+" ");
+            for (int k = 1; k < 10; k++) {
+                System.out.print(k+j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+}
