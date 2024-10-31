21f3cadcf986fd41810cfc79224c5a338c615adb
Fayoz-dev <frasulov988@gmail.com>
2024-10-31 21:30:59 +0500

    // 2^0+2^1+2^2+2^3+...

--- src/NestedLoop.java
+++ src/NestedLoop.java
@@ -18,7 +18,7 @@ public class NestedLoop {
         int n = 7;
         int sum = 0;
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             int pow = 1;
             for (int j = 1; j <= i; j++) {
                 pow *= x;
