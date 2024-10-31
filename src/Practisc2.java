1b1f48b48c9017fdb8aa76c83ebe4d442ee2cb3f
Fayoz-dev <frasulov988@gmail.com>
2024-10-31 22:53:16 +0500

    practics 2

---
+++ src/Practisc2.java
@@ -0,0 +1,17 @@
+public class Practisc2 {
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            System.out.print("(0," + j + ") ");
+        }
+        System.out.println();
+
+        for (int i = 1; i < 9; i++) {
+            System.out.println("(" + i + ",0)");
+        }
+
+        for (int j = 0; j < 10; j++) {
+            System.out.print("(9," + j + ") ");
+        }
+        System.out.println();
+    }
+}
