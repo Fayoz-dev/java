00e2d8b5c720525f5cbd4e9a6e038f0a008bab3c
Fayoz-dev <frasulov988@gmail.com>
2024-11-01 20:46:04 +0500

    1! + 2! + 3! +...+n!

---
+++ src/practicsNestedLoop/Practisc10.java
@@ -0,0 +1,16 @@
+package practicsNestedLoop;
+
+public class Practisc10 {
+    public static void main(String[] args) {
+        int n = 3;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            int faktorial = 1;
+            for (int j = 1; j <= i; j++) {
+                faktorial *= j;
+            }
+            sum += faktorial;
+        }
+        System.out.println(sum);
+    }
+}
