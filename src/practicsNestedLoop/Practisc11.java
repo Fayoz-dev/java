12f3ac5e809bf2f56aca466afed33d31de519636
Fayoz-dev <frasulov988@gmail.com>
2024-11-01 20:58:55 +0500

    A va B butun soni berilgan (A < B). A va B sonlari orasidagi barcha butun sonlarni chiqaruvchi programma tuzilsin. Bunda har bir son o'zining qiymaticha chiqarilsin. Ya'ni 3 soni 3 marta chiqariladi.

---
+++ src/practicsNestedLoop/Practisc11.java
@@ -0,0 +1,14 @@
+package practicsNestedLoop;
+
+public class Practisc11 {
+    public static void main(String[] args) {
+    int a=2;
+    int b=5;
+    for (int i=a+1; i<b;i++){
+        for (int j=0; j<i; j++){
+            System.out.println(i+" ");
+        }
+        System.out.println();
+      }
+    }
+}
