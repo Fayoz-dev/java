51f94d4c1e72bac0831f9157ff06b1a373717786
Fayoz-dev <frasulov988@gmail.com>
2024-11-01 21:55:45 +0500

    A va B butun soni berilgan (A < B). A va B sonlari orasidagi barcha butun sonlarni chiqaruvchi programma tuzilsin. Bunda A soni 1 marta, (A + 1) soni 2 marta, ( A+2) 3 marta chiqariladi

---
+++ src/practicsNestedLoop/Practisc12.java
@@ -0,0 +1,13 @@
+package practicsNestedLoop;
+
+public class Practisc12 {
+    public static void main(String[] args) {
+    int a=2;
+    int b=5;
+    for (int i=a; i<b;i++){
+         for (int j=0;j<(i-a+1);j++){
+          System.out.println(i);
+         }
+      }
+    }
+}
