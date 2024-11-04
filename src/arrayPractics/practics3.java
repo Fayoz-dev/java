0f38f6b14eddd67409fa749a8f49328fc7ce3f06
Fayoz-dev <frasulov988@gmail.com>
2024-11-04 21:31:34 +0500

    A[n-1], A[n-3], ... A[1].

---
+++ src/arrayPractics/practics3.java
@@ -0,0 +1,10 @@
+package arrayPractics;
+
+public class practics3 {
+    public static void main(String[] args) {
+     int[] numbers = {3,12,15,16,14};
+     for (int i = 0; i < numbers.length; i=i+2) {
+             System.out.println(numbers[numbers.length-1-i]);
+       }
+    }
+}
