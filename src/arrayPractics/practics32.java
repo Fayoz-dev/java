371a67f63d47001aaa50ee089da0d9152816d219
Fayoz-dev <frasulov988@gmail.com>
2024-11-04 21:48:13 +0500

    n ta elementdan tashkil topgan massiv berilgan. Massiv elementlari orasidan o'ng qo'shnisidan katta bo'lganlar sonini chiqaruvchi programma tuzilsin.

---
+++ src/arrayPractics/practics32.java
@@ -0,0 +1,12 @@
+package arrayPractics;
+
+public class practics32 {
+    public static void main(String[] args) {
+     int[] numbers = {3,12,15,16,14,13};
+     for (int i = 1; i < numbers.length; i++) {
+         if (numbers[i-1] > numbers[i]) {
+             System.out.println(numbers[i-1]);
+         }
+     }
+    }
+}
