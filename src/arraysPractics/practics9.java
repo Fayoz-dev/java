2a40f7d0f094212222ba34655e7028fef1ece203
Fayoz-dev <frasulov988@gmail.com>
2024-11-10 22:24:12 +0500

    n natural soni berilgan. Dastlabki n ta Fibonachchi sonlaridan tashkil topgan massivni hosil qiling va elementlarini chiqaring.
    F0 = 1; F1 = 1; F[k] = F[k-1] + F[k-2];  k=2, 3, 4, ...
    
    Namuna: n = 9
    
    Natija [0,1,1,2,3,5,8,13,21]

--- src/arraysPractics/practics9.java
+++ src/arraysPractics/practics9.java
@@ -2,15 +2,18 @@ package arraysPractics;
 
 public class practics9 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 7;
         int[] numbers = new int[n];
-        int num = 2;
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = num;
-            num*=3;
+        if (n > 0) numbers[0] = 0;
+        if (n > 1) numbers[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            numbers[i] = numbers[i - 1] + numbers[i - 2];
         }
-        for (int i = 0; i < numbers.length; i++) {
+
+        for (int i=0; i< numbers.length; i++) {
             System.out.println(numbers[i]);
         }
+
     }
 }
