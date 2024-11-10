2afd6244516f111753468a203ccd37f4ab40f9c8
Fayoz-dev <frasulov988@gmail.com>
2024-11-10 18:45:30 +0500

    array elementlarini kiritish chiqarish

---
+++ src/arrayPractics/arrayElementlariniKiritishChiqarish.java
@@ -0,0 +1,24 @@
+package arrayPractics;
+
+import java.util.Scanner;
+
+public class arrayElementlariniKiritishChiqarish {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hellomaleykum , Array uzunligini kirit: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        System.out.println("Array: ");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("array[" + i + "] = " + numbers[i]);
+            int a = scanner.nextInt();
+            numbers[i] = a;
+        }
+        System.out.println("Array Elementlari: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
---
+++ src/arrayPractics/practics5.java
@@ -0,0 +1,13 @@
+package arrayPractics;
+
+public class practics5 {
+    public static void main(String[] args) {
+     int[] numbers = {3,12,15,16,14};
+      int r = 5;
+      int min;
+      for (int i = 0; i < numbers.length; i++) {
+          min = numbers[i]-r;
+//          if (min < )
+      }
+    }
+}
