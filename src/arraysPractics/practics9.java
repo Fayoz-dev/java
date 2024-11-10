0ef67263048b542d64bb1885306a0a999528de1b
Fayoz-dev <frasulov988@gmail.com>
2024-11-10 21:50:39 +0500

    n natural soni va geometrik progressiyaning dastlabki hadi A va maxraji D berilgan.
      Geometrik progressiyaning dastlabki n ta hadidan tashkil topgan massivni hosil qiling va elementlarini chiqaring. A; = A * D
    
    Namuna: n= 5, A= 2,  D= 3
    
    Natija [2,6,18,54,162]

---
+++ src/arraysPractics/practics9.java
@@ -0,0 +1,16 @@
+package arraysPractics;
+
+public class practics9 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] numbers = new int[n];
+        int num = 2;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = num;
+            num*=3;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
